719a1f20ec7729c3f979a1b452788e0df6cd3f68
Ansh Singhal <singhal.ansh0503@gmail.com>
2024-01-23 14:00:50 +0530

    deployment

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from ultralytics import YOLO
 model = YOLO("best.pt")
 path = [["image1.jpg"], ["image2.jpg"]]
 
+
 def show_preds_image(image_path):
     image = cv2.imread(image_path)
     outputs = model.predict(source=image_path)
@@ -22,3 +23,21 @@ def show_preds_image(image_path):
             lineType=cv2.LINE_AA
         )
     return cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+
+input_images = [
+    gr.components.Image(type="filepath", label="Input Image"),
+]
+outputs_image = [
+    gr.components.Image(type="numpy", label="Output Image"),
+]
+interface_image = gr.Interface(
+    fn=show_preds_image,
+    inputs=input_images,
+    outputs=outputs_image,
+    title="Pascal Object Detection",
+    examples=path,
+    cache_examples=False,
+)
+
+gr.Interface(interface_image).queue().launch()
